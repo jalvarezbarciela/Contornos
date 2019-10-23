8965072b7ffd1e29cfe740a0404a7187cf7eeee0
Jose Alvarez Barciela <jalvarezbarciela@nautilus6.danielcastelao.org>
2019-10-23 13:22:05 +0200

    Primer commit

---
+++ src/contornos/Contornos.java
@@ -0,0 +1,14 @@
+package contornos;
+/**
+ * @author jalvarezbarciela
+ */
+public class Contornos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
